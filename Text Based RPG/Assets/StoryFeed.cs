5a1719acc92d60ffb7235309077c5890a33652ff
Jared Kneedler Mac <kneedler.jared@gmail.com>
2018-06-28 10:57:01 -0400

    initial project creation

---
+++ Text Based RPG/Assets/StoryFeed.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryFeed : MonoBehaviour {
+
+	public Queue<string> feed = new Queue<string> ();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			AddToFeed ("Yes");
+		}
+	}
+
+	public void AddToFeed(string line){
+		feed.Enqueue (line);
+	}
+}
